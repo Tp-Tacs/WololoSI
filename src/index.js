58aeb653d033557be3e76770c747bf269795a689
CimimUxMaio <cimimuxmaio@gmail.com>
2020-09-20 14:01:39 -0300

    Ejemplo websockets

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
-const io = require("socket.io");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const serverPort = 8080;
+
+io.on("connection", (socket) => {
+  console.log("Client connected via websocket!");
+  io.emit("greeting", "Buen dia :D, soy el socket " + socket.id);
+});
+
+app.use(express.static("test_client"));
 
 app.get("/", (_req, res) => {
   res.send("Hola mundo!");
 });
 
-app.listen(serverPort, () => {
+server.listen(serverPort, () => {
   console.log("Server online...");
 });
